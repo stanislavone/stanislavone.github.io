8c51f6e197fd25e8189e924b65d1313fc072ec29
Stanislav Farfudinov <s4day88@yandex.ru>
2019-01-27 16:47:18 +0500

    Add files via upload
    Замена значений в массиве

---
+++ arr.js
@@ -0,0 +1,9 @@
+var style = ['Джаз', 'Блюз'];
+console.log(style);
+style.push('Рок-н-Ролл');
+console.log(style);
+style[1] = 'Classic';
+console.log(style);
+style.shift();
+style.unshift('Rap', 'Raggy');
+console.log(style);
