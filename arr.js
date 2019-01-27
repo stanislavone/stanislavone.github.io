9ef30eba567d2c7956b37f51015a3da24fe3abee
Stanislav Farfudinov <s4day88@yandex.ru>
2019-01-27 16:58:18 +0500

    Update arr.js

--- arr.js
+++ arr.js
@@ -2,7 +2,7 @@ var style = ['Джаз', 'Блюз'];
 console.log(style);
 style.push('Рок-н-Ролл');
 console.log(style);
-style[1] = 'Classic';
+style = style.slice(1, 1, 'Классик');
 console.log(style);
 style.shift();
 style.unshift('Rap', 'Raggy');
