aacd51c0490cf55bd03967409379f74748120596
Stanislav Farfudinov <s4day88@yandex.ru>
2019-12-18 23:01:09 +0500

    Delete arr.js

--- arr.js
+++
@@ -1,9 +0,0 @@
-var style = ['Джаз', 'Блюз'];
-console.log(style);
-style.push('Рок-н-Ролл');
-console.log(style);
-style.splice(1, 1, 'Классик');
-console.log(style);
-style.shift();
-style.unshift('Rap', 'Raggy');
-console.log(style);
