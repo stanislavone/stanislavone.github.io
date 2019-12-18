3036592714ae6a6c3e3e5e280fe55cfe9088570e
Stanislav Farfudinov <s4day88@yandex.ru>
2019-12-18 23:02:53 +0500

    Delete arr2.js

--- arr2.js
+++
@@ -1,7 +0,0 @@
-// Cлучайное значение из массива
-
-var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
-var min = arr.length - arr.length;
-var max = arr.length;
-var rand = min + Math.floor(Math.random() * (max + 1 - min));
-console.log(arr[rand]);
