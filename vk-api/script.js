381711ae5447b20958695384bf0d4229ca203adc
stanislavone <s4day88@yandex.ru>
2020-10-05 17:21:09 +0500

    ddd

--- vk-api/script.js
+++ vk-api/script.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
         var count = r.response.count; // кол-во полученных фотографий
         var items = r.response.items; // массив с фотографиями
 
-        console.log(count, items.photo_75);
+        console.log(count, items.count.photo_75);
         
 
  
