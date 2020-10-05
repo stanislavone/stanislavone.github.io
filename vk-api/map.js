12984041ef503276b30bf0764650c9b3b198c821
stanislavone <s4day88@yandex.ru>
2020-10-05 20:35:54 +0500

    ggg

--- vk-api/map.js
+++ vk-api/map.js
@@ -1,23 +1,49 @@
 $(document).ready(function () { 
     ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 7
+    var myMap;
+    
+    function init () {
+        myMap = new ymaps.Map("map", {
+            center: [57.5262, 38.3061], // Углич
+            zoom: 11
+        }, {
+            balloonMaxWidth: 200,
+            searchControlProvider: 'yandex#search'
+        });
+    
+        // Обработка события, возникающего при щелчке
+        // левой кнопкой мыши в любой точке карты.
+        // При возникновении такого события откроем балун.
+        myMap.events.add('click', function (e) {
+            if (!myMap.balloon.isOpen()) {
+                var coords = e.get('coords');
+                myMap.balloon.open(coords, {
+                    contentHeader:'Событие!',
+                    contentBody:'<p>Кто-то щелкнул по карте.</p>' +
+                        '<p>Координаты щелчка: ' + [
+                        coords[0].toPrecision(6),
+                        coords[1].toPrecision(6)
+                        ].join(', ') + '</p>',
+                    contentFooter:'<sup>Щелкните еще раз</sup>'
+                });
+            }
+            else {
+                myMap.balloon.close();
+            }
+        });
+    
+        // Обработка события, возникающего при щелчке
+        // правой кнопки мыши в любой точке карты.
+        // При возникновении такого события покажем всплывающую подсказку
+        // в точке щелчка.
+        myMap.events.add('contextmenu', function (e) {
+            myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+        });
+        
+        // Скрываем хинт при открытии балуна.
+        myMap.events.add('balloonopen', function (e) {
+            myMap.hint.close();
         });
     }
-
-    myMap.events.add('click', function (e) {
-        // Получение координат щелчка
-        var coords = e.get('coords');
-        console.log(coords.join(', '));
-    });
 
 })
