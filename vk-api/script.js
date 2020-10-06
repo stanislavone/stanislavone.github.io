af3cbcf36cfc45d7395a43fd307f3e0671050184
stanislavone <s4day88@yandex.ru>
2020-10-06 10:43:47 +0500

    jj

--- vk-api/script.js
+++ vk-api/script.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
                         for(let i = 0; i < count-1; i++) {
                          
                           console.log(items[i].photo_75, "5");
-                          $("ul").append("<a href="+items[i].photo_1280+" target='blank'><img src="+items[i].photo_604+"></a>");
+                          $("ul").append("<a href="+items[i].photo_1280+" target='blank'><img src="+items[i].photo_130+"></a>");
                       }
           
                     }
